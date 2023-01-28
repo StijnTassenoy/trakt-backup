03448a91aa2b0e146010f736d35668adcab16cba
StijnTassenoy <stijn.tassenoy@gmail.com>
2023-01-28 13:49:01 +0100

    Refactored code + added xlsx export

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-import itertools
 import os
 import time
+import itertools
+from typing import List, Dict
 
 import dotenv
-import pandas as pd
 import requests
+import pandas as pd
 
 dotenv.load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 
 
-def authenticate_trakt():
+def authenticate_trakt() -> None:
     client_secret = os.getenv("CLIENT_SECRET")
     headers = {"Content-Type": "application/json"}
 
@@ -40,7 +41,7 @@ def authenticate_trakt():
     dotenv.set_key(".env", "REFRESH_TOKEN", r["refresh_token"])
 
 
-def get_history(page: int):
+def get_history_page(page: int) -> List[List[Dict]]:
     limit = 100
     headers = {
         "Content-Type": "application/json",
@@ -52,54 +53,80 @@ def get_history(page: int):
     return history
 
 
+def get_complete_history() -> List[List[Dict] | List[List[Dict]]]:
+    idx = 1
+    complete_history = []
+    while True:
+        page_history = get_history_page(idx)
+        if page_history:
+            # print(page_history)
+            complete_history.append(page_history)
+        else:
+            break
+        idx += 1
+
+    complete_history = list(itertools.chain.from_iterable(complete_history))
+    return complete_history
+
+
+def restructure_data(data) -> List[Dict]:
+    restructured_data = []
+    for d in data:
+        video_type = d["type"]
+
+        structured_data = {
+            "id": d["id"],
+            "watched_at": d["watched_at"],
+            "type": video_type,
+            "title": d[video_type]["title"],
+            "trakt_id": d[video_type]["ids"]["trakt"],
+            "imdb_id": d[video_type]["ids"]["imdb"],
+            "tmdb_id": d[video_type]["ids"]["tmdb"],
+            "action": d["action"],
+        }
+
+        #TODO: typing
+        if video_type == "episode":
+            structured_data["year"] = d["show"]["year"]
+            structured_data["show_name"] = d["show"]["title"]
+            structured_data["episode_season"] = d[video_type]["season"]
+            structured_data["episode_number"] = d[video_type]["number"]
+            structured_data["slug"] = d["show"]["ids"]["slug"]
+            structured_data["show_trakt_id"] = d["show"]["ids"]["trakt"]
+            structured_data["show_tvdb_id"] = d["show"]["ids"]["tvdb"]
+            structured_data["show_imdb_id"] = d["show"]["ids"]["imdb"]
+            structured_data["show_tmdb_id"] = d["show"]["ids"]["tmdb"]
+        else:
+            structured_data["year"] = d[video_type]["year"]
+            structured_data["show_name"] = None
+            structured_data["episode_season"] = None
+            structured_data["episode_number"] = None
+            structured_data["slug"] = d[video_type]["ids"]["slug"]
+            structured_data["show_trakt_slug"] = None
+            structured_data["show_trakt_id"] = None
+            structured_data["show_tvdb_id"] = None
+            structured_data["show_imdb_id"] = None
+            structured_data["show_tmdb_id"] = None
+
+        restructured_data.append(structured_data)
+    return restructured_data
+
+
 def main():
     if int(time.time()) > int(os.getenv("ACTIVATION_EPOCH")) + int(os.getenv("EXPIRES_IN")) - 86400:
         authenticate_trakt()
     else:
-        full_history = []
-        idx = 1
-        while True:
-            page_history = get_history(idx)
-            if page_history:
-                print(page_history)
-                full_history.append(page_history)
-            else:
-                break
-            idx += 1
-        full_history = list(itertools.chain.from_iterable(full_history))
-        pd.options.display.max_columns = None
-        df = pd.DataFrame.from_records(full_history, columns=["id", "watched_at", "action", "type", "video_data", "extra"])
-        print(df.head())
-
-        new_data = []
-
-        # iterate through each dictionary in the list
-        for d in full_history:
-            # extract the relevant information
-            id = d['id']
-            watched_at = d['watched_at']
-            action = d['action']
-            type = d['type']
-            video_data = d[type]
-            if type == 'episode':
-                video_data.update(d['show'])
-            # add the extracted information to the new list
-            new_data.append({
-                "id": id,
-                "watched_at": watched_at,
-                "action": action,
-                "type": type,
-                "video_data": video_data
-            })
-
-        # create a DataFrame from the new list of dictionaries
-        df = pd.DataFrame.from_records(new_data, columns=["id", "watched_at", "action", "type", "video_data", "show_data"])
-
-        # display the DataFrame
-        print()
+        complete_history = get_complete_history()
+        restructured_history = restructure_data(complete_history)
+        df = pd.DataFrame.from_records(restructured_history)
+        df["year"] = df["year"]
+        df["episode_season"] = df["episode_season"].apply(lambda x: str(x).rstrip("0").rstrip(".") if "." in str(x) else x)
+        df["episode_number"] = df["episode_number"].apply(lambda x: str(x).rstrip("0").rstrip(".") if "." in str(x) else x)
+        df["show_trakt_id"] = df["show_trakt_id"].apply(lambda x: str(x).rstrip("0").rstrip(".") if "." in str(x) else x)
+        df["show_tvdb_id"] = df["show_tvdb_id"].apply(lambda x: str(x).rstrip("0").rstrip(".") if "." in str(x) else x)
+        df["show_tmdb_id"] = df["show_tmdb_id"].apply(lambda x: str(x).rstrip("0").rstrip(".") if "." in str(x) else x)
         print(df)
-        print()
-        print(df.head())
+        df.to_excel("./trakt_history.xlsx", sheet_name="Trakt")
 
 
 if __name__ == "__main__":
